e7346d03431194789a952edf44e79729f1328c51
stones Zheng <eezhenglei@126.com>
2021-03-04 22:35:27 +0800

    adapt to flutter 2.0

--- lib/src/chewie_player.dart
+++ lib/src/chewie_player.dart
@@ -82,7 +82,6 @@ class ChewieState extends State<Chewie> {
       Animation<double> animation,
       _ChewieControllerProvider controllerProvider) {
     return Scaffold(
-      resizeToAvoidBottomPadding: false,
       body: Container(
         alignment: Alignment.center,
         color: Colors.black,
@@ -280,7 +279,7 @@ class ChewieController extends ChangeNotifier {
 
   static ChewieController of(BuildContext context) {
     final chewieControllerProvider =
-        context.inheritFromWidgetOfExactType(_ChewieControllerProvider)
+        context.dependOnInheritedWidgetOfExactType(aspect: _ChewieControllerProvider)
             as _ChewieControllerProvider;
 
     return chewieControllerProvider.controller;
